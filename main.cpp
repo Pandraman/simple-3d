7f4b01d4045a5b20896f7f2826f8f4fb061c361c
Pandraman <92603994+Pandraman@users.noreply.github.com>
2023-05-23 21:55:24 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -45,7 +45,8 @@ void drawTexturedShape(const std::vector<sf::Vector2f>& vertices, const std::vec
 
 RenderWindow window(VideoMode(1080,720),"Window");
 
-Vector2i cameraRotation;
+Vector2f cameraRotation;
+Vector3f cameraPosition;
 void drawRect(sf::RenderWindow& window, const sf::Vector2f& position, const sf::Vector2f& size, Vector3i color) {
     sf::RectangleShape rect(size);
     rect.setPosition(position);
@@ -55,25 +56,24 @@ void drawRect(sf::RenderWindow& window, const sf::Vector2f& position, const sf::
 }
 
 
-Vector3i rotate(float x, float y, float z, float xrot, float yrot, float zrot) {
-    float X = x;
-    float Y = y;
-    float Z = z;
+Vector3f rotate_y(const Vector3f& pos, const Vector3f& b, float xrot) {
+    Vector3f c;
+    float cosAngle = cos(xrot);
+    float sinAngle = sin(xrot);
 
-    //xrot
-    Y = (y*sin(xrot)+z*cos(xrot));
-    Z = (z*sin(xrot)+y*cos(xrot));
+    c.x = (pos.x - b.x) * cosAngle + (pos.z - b.z) * sinAngle + b.x;
+    c.z = (pos.z - b.z) * cosAngle - (pos.x - b.x) * sinAngle + b.z;
+    c.y = pos.y;
 
-    // yrot
-    X = (x*sin(yrot)+Z*cos(yrot));
-    Z = (Z*sin(yrot)+x*cos(yrot));
-
-    //zrot
-    X = (X*sin(zrot)+Y*cos(zrot));
-    Y = (Y*sin(zrot)+X*cos(zrot));
-
-    return Vector3i(X,Y,Z);
+    return c;
 }
+
+
+
+
+
+
+
 
 
 
@@ -92,7 +92,7 @@ public:
 }
 
 
-    void draw(const sf::Texture& texture, sf::RenderWindow& window, float) const {
+    void draw(const sf::Texture& texture, sf::RenderWindow& window) const {
         std::vector<sf::Vector2f> vertices;
 
         for (auto& position : positions) {
@@ -115,34 +115,6 @@ public:
     std::vector<sf::Vector3f> vertices;
     std::vector<sf::Vector2f> texCoords;
     
-    Vector3f rotate(const Vector3f& point, float xrot, float yrot, float zrot) {
-        float x = point.x;
-        float y = point.y;
-        float z = point.z;
-
-        // xrot
-        float newY = y * std::cos(xrot) - z * std::sin(xrot);
-        float newZ = y * std::sin(xrot) + z * std::cos(xrot);
-
-        // yrot
-        float newX = x * std::cos(yrot) + newZ * std::sin(yrot);
-        newZ = -x * std::sin(yrot) + newZ * std::cos(yrot);
-
-        // zrot
-        float newX2 = newX * std::cos(zrot) - newY * std::sin(zrot);
-        float newY2 = newX * std::sin(zrot) + newY * std::cos(zrot);
-
-        return Vector3f(newX2, newY2, newZ);
-    }
-
-    void rotate(float xrot, float yrot, float zrot) {
-        for (auto& face : faces) {
-            for (auto& position : face.positions) {
-                position = rotate(position, xrot, yrot, zrot);
-            }
-        }
-    }
-
     bool isLoaded() const {
         return !faces.empty();
     }
@@ -198,21 +170,44 @@ public:
         file.close();
     }
     
-    void draw(const sf::Vector3f& cameraPosition, sf::Texture& texture, sf::RenderWindow& window) {
-        // Calculate the center of each face and face normals
-        
+    void draw(const sf::Vector3f& cameraPosition, const sf::Vector2f& cameraRotation, sf::Texture& texture, sf::RenderWindow& window) {
         sf::Vector2u windowSize = window.getSize();
         float halfWidth = windowSize.x / 2.0f;
         float halfHeight = windowSize.y / 2.0f;
         sf::View view(sf::FloatRect(-halfWidth, -halfHeight, windowSize.x, windowSize.y));
         window.setView(view);
-        
-        for (auto& face : faces) {
-            face.calculateCenter();
+
+        std::vector<Face> visibleFaces;
+
+        for (const auto& face : faces) {
+            std::vector<sf::Vector3f> modified;
+            bool isVisible = false;
+
+            for (const auto& vertex : face.positions) {
+                sf::Vector3f pos = vertex;
+                sf::Vector3f mod_pos = rotate_y(pos, cameraPosition, cameraRotation.x) - cameraPosition;
+
+                // Check if the vertex is in front of the camera
+                sf::Vector3f diff = mod_pos - cameraPosition;
+                float distance = std::sqrt(diff.x * diff.x + diff.y * diff.y + diff.z * diff.z);
+                if (distance > 1.f && diff.z < 0) {
+                    isVisible = true;
+                }
+
+                modified.push_back(mod_pos);
+            }
+
+            if (isVisible) {
+                Face visibleFace;
+                visibleFace.positions = modified;
+                visibleFace.texCoords = face.texCoords;
+                visibleFace.calculateCenter();
+                visibleFaces.push_back(visibleFace);
+            }
         }
 
-        // Sort the faces by distance from camera position
-        std::sort(faces.begin(), faces.end(), [&cameraPosition](const Face& a, const Face& b) {
+        // Sort the visible faces by distance from camera position
+        std::sort(visibleFaces.begin(), visibleFaces.end(), [&cameraPosition](const Face& a, const Face& b) {
             sf::Vector3f diffA = a.center - cameraPosition;
             sf::Vector3f diffB = b.center - cameraPosition;
             float distA = std::sqrt(diffA.x * diffA.x + diffA.y * diffA.y + diffA.z * diffA.z);
@@ -220,71 +215,12 @@ public:
             return distA > distB; // Sort in descending order (farther faces first)
         });
 
-
-        for (auto& face : faces) {
-        for (auto& position : face.positions) {
-            // Apply camera rotation around the y-axis (yaw)
-            float x = position.x - cameraPosition.x;
-            float z = position.z - cameraPosition.z;
-            float rotatedX = x * std::cos(cameraRotation.y) - z * std::sin(cameraRotation.y);
-            float rotatedZ = x * std::sin(cameraRotation.y) + z * std::cos(cameraRotation.y);
-            position.x = rotatedX + cameraPosition.x;
-            position.z = rotatedZ + cameraPosition.z;
-            
-            // Apply camera rotation around the x-axis (pitch)
-            float y = position.y - cameraPosition.y;
-            float rotatedY = y * std::cos(cameraRotation.x) - rotatedZ * std::sin(cameraRotation.x);
-            float rotatedZ2 = y * std::sin(cameraRotation.x) + rotatedZ * std::cos(cameraRotation.x);
-            position.y = rotatedY + cameraPosition.y;
-            position.z = rotatedZ2 + cameraPosition.z;
-        }
-    }
-
-
-        // Draw the sorted faces
-        for (const auto& face : faces) {
-        std::vector<sf::Vector2f> vertices;
-
-        for (const auto& position : face.positions) {
-            sf::Vector3f modifiedPosition = position - cameraPosition;
-            modifiedPosition.z *= -1;
-            sf::Vector2f position2D = face.to2D(modifiedPosition);
-            
-            vertices.push_back(position2D);
-        }
-
-        // Draw the face with the converted 2D vertices
-        drawTexturedShape(vertices, face.texCoords, texture, window);
+        for (const auto& face : visibleFaces) {
+            face.draw(texture, window);
         }
     }
     
 };
-
-
-
-
-sf::Vector3f cameraPosition(0.0f, 0.0f, 0.0f);
-sf::Vector2i prevMousePos(0, 0);
-sf::Vector2i mouseDelta(0, 0);
-
-void updateCameraRotation(const sf::Vector2i& mouseDelta) {
-    float rotationSpeed = 0.1f;
-
-    // Update camera rotation based on mouse movement
-    float yaw = mouseDelta.x * rotationSpeed;
-    float pitch = mouseDelta.y * rotationSpeed;
-
-    // Apply rotation to camera
-    cameraRotation.x += pitch/20;
-    cameraRotation.y += yaw/20;
-
-    // Clamp pitch to avoid flipping upside down
-    constexpr float maxPitch = 89.0f;
-    constexpr float minPitch = -89.0f;
-}
-
-
-
 
 
 int main()
@@ -292,29 +228,12 @@ int main()
     
     //SETUP
 
-    
-    bool isWKeyPressed = false;
-    bool isAKeyPressed = false;
-    bool isSKeyPressed = false;
-    bool isDKeyPressed = false;
-    bool isSpaceKeyPressed = false;
-    bool isShiftKeyPressed = false;
-    
-
-
-
     Model model;
-    model.loadFromObj("suzanne.obj");
+    model.loadFromObj("Object/suzanne.obj");
     cout << model.isLoaded() << endl;
     Texture text;
-    float angleY = 45; // Rotation angle in degrees on the Y axis
-    float angleX = 45; // Rotation angle in degrees on the X axis
 
-    float radiansY = angleY * (3.14159f / 180.0f); // Convert angle to radians
-    float radiansX = angleX * (3.14159f / 180.0f); // Convert angle to radians
-
-    text.loadFromFile("D:\\Desktop/Suzanne/monkey.png");
-    sf::Vector3f cameraPosition(0.0f, 0.0f, -10.0f); // Example camera position
+    text.loadFromFile("Object/monkey.png");
 
     while (window.isOpen())
     {
@@ -323,101 +242,51 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
-            else if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::W) {
-                    isWKeyPressed = true;
-                }
-                else if (event.key.code == sf::Keyboard::A) {
-                    isAKeyPressed = true;
-                }
-                else if (event.key.code == sf::Keyboard::S) {
-                    isSKeyPressed = true;
-                }
-                else if (event.key.code == sf::Keyboard::D) {
-                    isDKeyPressed = true;
-                }
-                else if (event.key.code == sf::Keyboard::Space) {
-                    isSpaceKeyPressed = true;
-                }
-                else if (event.key.code == sf::Keyboard::LShift || event.key.code == sf::Keyboard::RShift) {
-                    isShiftKeyPressed = true;
-                }
+
             }
-            else if (event.type == sf::Event::KeyReleased) {
-                if (event.key.code == sf::Keyboard::W) {
-                    isWKeyPressed = false;
-                }
-                else if (event.key.code == sf::Keyboard::A) {
-                    isAKeyPressed = false;
-                }
-                else if (event.key.code == sf::Keyboard::S) {
-                    isSKeyPressed = false;
-                }
-                else if (event.key.code == sf::Keyboard::D) {
-                    isDKeyPressed = false;
-                }
-                else if (event.key.code == sf::Keyboard::Space) {
-                    isSpaceKeyPressed = false;
-                }
-                else if (event.key.code == sf::Keyboard::LShift || event.key.code == sf::Keyboard::RShift) {
-                    isShiftKeyPressed = false;
-                }
-            }
-            else if (event.type == sf::Event::MouseMoved) {
-                // Calculate mouse delta based on previous position
-                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                mouseDelta = mousePos - prevMousePos;
-                updateCameraRotation(mouseDelta);
-                prevMousePos = mousePos;
 
-                // Update camera rotation based on mouse input
-
-                // Store current mouse position as previous position for the next frame
-            }
-            }
-        window.clear();
-        
-
-
-        // Handle the input actions based on the key states
-        if (isWKeyPressed) {
-            cameraPosition.z += .005;
-        }
-        if (isAKeyPressed) {
-            cameraPosition.x += .005;
-        }
-        if (isSKeyPressed) {
-            cameraPosition.z -= .005;
-        }
-        if (isDKeyPressed) {
-            cameraPosition.x -= .005;
-        }
-        if (isSpaceKeyPressed) {
-            cameraPosition.y += .005;
-        }
-        if (isShiftKeyPressed) {
-            cameraPosition.y -= .005;
-        }
-        
-       
-       
-       
-        model.draw(cameraPosition, text, window);
-        //std::vector<Vector2f> verts = {a.To2D(), b.To2D(), d.To2D(), c.To2D()};
-        //std::vector<Vector2f> tex = {Vector2f(0,0),Vector2f(1,0),Vector2f(1,1),Vector2f(0,1)};
-        //sf::Texture Tex;
-        //Tex.loadFromFile("D:/Desktop/Suzanne/monkey.png");
-        //drawTexturedShape(verts, tex, Tex, window);
-        
-        mouseDelta = Vector2i(0,0);
-    
-
-        
-        
-
-
-        window.display();
+    if (Keyboard::isKeyPressed(Keyboard::Key::W)) {
+        cameraPosition.z -= .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::S)) {
+        cameraPosition.z += .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
+        cameraPosition.x += .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
+        cameraPosition.x -= .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::LShift)) {
+        cameraPosition.y -= .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::Space)) {
+        cameraPosition.y += .01;
     }
 
-    return 0;
-}
+    if (Keyboard::isKeyPressed(Keyboard::Key::Up)) {
+        cameraRotation.y += .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::Down)) {
+        cameraRotation.y -= .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::Left)) {
+        cameraRotation.x += .01;
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Key::Right)) {
+        cameraRotation.x -= .01;
+    }
+
+
+
+
+        window.clear();
+
+
+
+
+
+        model.draw(cameraPosition, cameraRotation, text, window);
+
+        window.display();
+    }return 0;}
